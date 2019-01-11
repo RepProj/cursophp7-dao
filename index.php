ae4c51c818954323f2ce1a3e82e998f0676e1f29
Alisson Mendes <alisson.mnds@gmail.com>
2019-01-11 00:51:21 -0200

    metodos lista

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-$root->loadById(3);
-echo $root;
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
+
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+$usuario = new Usuario();
+$usuario->login("root", "!1@#$");
+echo $usuario;
 
 ?>
